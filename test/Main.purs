7c1325807a6c3020993c9a4c2c736ee5cf9356b3
Marco Sampellegrini <babbonatale@alpacaaa.net>
2017-10-01 08:14:27 +0000

    Quickcheck tests.

--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,33 @@
 module Test.Main where
 
+import Crypto.Hash.MerkleTree as MerkleTree
+
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff.Random (RANDOM)
+import Control.Monad.Eff.Exception (EXCEPTION)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "You should add some tests."
+
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Show (genericShow)
+import Test.QuickCheck (quickCheck)
+import Test.QuickCheck.Arbitrary (class Arbitrary)
+import Test.QuickCheck.Gen (chooseInt)
+
+newtype MerkleTest = MerkleTest { size :: Int, leaf :: Int }
+
+derive instance genericMerkleTest :: Generic MerkleTest _
+
+instance showMerkleTest :: Show a => Show MerkleTest where
+  show = genericShow
+
+instance arbitraryMerkleTest :: Arbitrary MerkleTest where
+  arbitrary = do
+    size <- chooseInt 100 10000
+    leaf <- chooseInt 100 size
+    pure $ MerkleTest { size, leaf }
+
+main :: Eff (console :: CONSOLE, random :: RANDOM, exception :: EXCEPTION) Unit
+main = quickCheck \(MerkleTest { size, leaf }) ->
+  MerkleTree.testMerkleProofN size leaf
